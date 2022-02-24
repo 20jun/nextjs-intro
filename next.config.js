ea3c435c99f7c1d45e199a81df1cc543745c4baa
20jun <ex_xe@naver.com>
2022-02-24 20:07:17 +0900

    #2.3 getServerProps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const API_KEY = "587385775810ebf84f881974e971b194";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,57 @@
-import NavBar from "../components/NavBar";
+import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import Seo from "../components/Seo";
 
-export default function Home() {
+const API_KEY = "587385775810ebf84f881974e971b194";
+
+export default function Home({ results }) {
+  const [movies, setMovies] = useState();
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <Seo title="Home" />
+
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  // 서버에서 돌아감 server쪽에서만
+  // 백엔드에서만 실행
+  // 여기서 무엇을 return 하던지, 이걸 props로써 page에게 주게 됨
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
