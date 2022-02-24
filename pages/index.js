eac9f59ff200b18d657854dbd854b9ef7e6de1a0
20jun <ex_xe@naver.com>
2022-02-24 21:13:59 +0900

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 import Seo from "../components/Seo";
@@ -5,16 +7,26 @@ import Seo from "../components/Seo";
 const API_KEY = "587385775810ebf84f881974e971b194";
 
 export default function Home({ results }) {
-  const [movies, setMovies] = useState();
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
 
   return (
     <div className="container">
       <Seo title="Home" />
-
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -23,6 +35,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
